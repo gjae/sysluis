42bc93a17233d53220b433ec6ddc1d43b37c634a
Genesis Chirinos <genesisvirgia22@gmail.com>
2017-08-19 17:50:50 -0400

    arreglado el error restante en el formulario de registro de clientes

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -12,15 +12,13 @@
     }
 
     function soloNumeros(evento, input, longitud){
-        alert(evento.keyCode)
         var key = (evento.keyCode == 9 && evento.keyCode != 8 && evento.keyCode != 38 && evento.keyCode != 39 && evento.keyCode != 40 && evento.keyCode != 37   )
-        if( !(/^[0-9]+$/.test( input.value )) || key ){
+        if( !(/^[0-9]+$/.test( input.value )) || input.value.length > longitud && key ){
             alert("EL CAMPO QUE INTENTA USAR SOLO PUEDE POSEER CARACTERES NUMERICOS Y DEBE TENER UNA LONGITUD MAXIMA DE "+longitud+ " CARACTERES")
             input.value = input.value.substring(0 , (input.value.length - 1) )
         }
         if(input.value.length > longitud)
             input.value = input.value.substring(0, (input.value.length - 1))
-
     }
 
 function validarCedula(cedula, longitud)
